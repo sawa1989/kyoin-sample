c3b3e8ff8d5fb5e6026cfa53ce8775b6b6a16eba
kyoin <gin9815@gmail.com>
2024-09-01 21:50:52 +0900

    리포지토리 개발

--- src/main/kotlin/com/kyoin/portfolio/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/kyoin/portfolio/presentation/repository/PresentationRepository.kt
@@ -1,6 +1,39 @@
 package com.kyoin.portfolio.presentation.repository
 
+import com.kyoin.portfolio.domain.entity.*
+import com.kyoin.portfolio.domain.repository.*
 import org.springframework.stereotype.Repository
 
 @Repository
-class PresentationRepository
+class PresentationRepository(
+    private val achievementRepository: AchievementRepository,
+    private val introductionRepository: IntroductionRepository,
+    private val linkRepository: LinkRepository,
+    private val skillRepository: SkillRepository,
+    private val projectRepository: ProjectRepository,
+    private val experienceRepository: ExperienceRepository
+){
+    fun getActiveAchievements(): List<Achievement> {
+        return achievementRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveExperiences(): List<Experience> {
+        return experienceRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveIntroductions(): List<Introduction> {
+        return introductionRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveLinks(): List<Link> {
+        return linkRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveProjects(): List<Project> {
+        return projectRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveSkills(): List<Skill> {
+        return skillRepository.findAllByIsActive(true)
+    }
+}
