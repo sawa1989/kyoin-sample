49ac79bcf07dee6e1f382a165c13541be00ce53e
kyoin <gin9815@gmail.com>
2024-09-06 22:56:16 +0900

    인터셉터 // 화면

--- src/main/kotlin/com/kyoin/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/kyoin/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,6 +1,24 @@
 package com.kyoin.portfolio.presentation.interceptor
 
+import com.kyoin.portfolio.domain.entity.HttpInterface
+import com.kyoin.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+): HandlerInterceptor {
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface) // 단건 entity 저장
+    }
+}
