2ec9c93b78f8dce51d58b65bbcc8c68a00b4553d
kyoin <gin9815@gmail.com>
2024-09-08 19:49:57 +0900

    api 개발

--- src/main/kotlin/com/kyoin/portfolio/admin/context/project/service/AdminProjectSkillService.kt
+++ src/main/kotlin/com/kyoin/portfolio/admin/context/project/service/AdminProjectSkillService.kt
@@ -1,18 +1,22 @@
 package com.kyoin.portfolio.admin.context.project.service
 
+import com.kyoin.portfolio.admin.context.project.form.ProjectSkillForm
 import com.kyoin.portfolio.admin.data.TableDTO
-import com.kyoin.portfolio.domain.entity.Skill
+import com.kyoin.portfolio.admin.exception.AdminBadReqeustException
+import com.kyoin.portfolio.admin.exception.AdminInternalServerErrorException
+import com.kyoin.portfolio.domain.entity.ProjectSkill
 import com.kyoin.portfolio.domain.repository.ProjectRepository
+import com.kyoin.portfolio.domain.repository.ProjectSkillRepository
 import com.kyoin.portfolio.domain.repository.SkillRepository
-import jakarta.transaction.TransactionScoped
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class AdminProjectSkillService (
+class AdminProjectSkillService(
     private val projectRepository: ProjectRepository,
-    private val skillRepository: SkillRepository
-){
+    private val skillRepository: SkillRepository,
+    private val projectSkillRepository: ProjectSkillRepository
+) {
     @Transactional
     fun getProjectSkillTable(): TableDTO {
         val projects = projectRepository.findAll()
@@ -34,18 +38,57 @@ class AdminProjectSkillService (
                 records.add(record)
             }
         }
-        return TableDTO(name="ProjectSkill", columns = columns, records = records)
+        return TableDTO(name = "ProjectSkill", columns = columns, records = records)
     }
 
-    fun getProjectList(): List<String>{
+    fun getProjectList(): List<String> {
         val projects = projectRepository.findAll()
 
-        return projects.map {"${it.id} ( ${it.name})"}.toList()
+        return projects.map { "${it.id} ( ${it.name})" }.toList()
     }
 
     fun getSkillList(): List<String> {
         val skills = skillRepository.findAll()
 
-        return skills.map {"${it.id} ( ${it.name})"}.toList()
+        return skills.map { "${it.id} ( ${it.name})" }.toList()
+    }
+
+
+    @Transactional
+    fun save(form: ProjectSkillForm) {
+
+        // 이미 매핑된 Project - Skill 여부 검증
+        val projectId = parseId(form.project)
+        val skillId = parseId(form.skill)
+        projectSkillRepository.findByProjectIdAndSkillId(projectId, skillId)
+            .ifPresent { throw AdminBadReqeustException("이미 매핑된 데이터입니다.") }
+
+        // 유효한 ProjectSkill 생성
+        val project = projectRepository.findById(projectId)
+            .orElseThrow { throw AdminBadReqeustException("ID ${projectId}에 해당하는 데이터를 찾을 수 없습니다.") }
+        val skill = skillRepository.findById(skillId)
+            .orElseThrow { throw AdminBadReqeustException("ID ${skillId}에 해당하는 데이터를 찾을 수 없습니다.") }
+        val projectSkill = ProjectSkill(
+            project = project,
+            skill = skill
+        )
+
+        project.skills.add(projectSkill)
+    }
+
+    private fun parseId(line: String): Long {
+        try {
+            val endIndex = line.indexOf(" ") - 1
+            val id = line.slice(0..endIndex).toLong()
+
+            return id
+        } catch (e: Exception) {
+            throw AdminInternalServerErrorException("ID 추출 중 오류가 발생했습니다.")
+        }
+    }
+
+    @Transactional
+    fun delete(id: Long) {
+        projectSkillRepository.deleteById(id)
     }
 }
